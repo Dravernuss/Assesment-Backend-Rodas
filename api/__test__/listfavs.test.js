d298f4c6fd453460ca77ae178087c95ddaae6f79
Esteban <esteban16.rodas@gmail.com>
2022-05-04 12:30:13 -0500

    adding errors testing

--- api/__test__/listfavs.test.js
+++ api/__test__/listfavs.test.js
@@ -41,6 +41,28 @@ describe("ListFav tests", () => {
     expect(result.data.newList.favs).toBeDefined();
   });
 
+  it("should fail creating a new list for missing token", async () => {
+    const favList = {
+      name: "Movies",
+      user_id: "6255a4e06a9793184425c922",
+      favs: [
+        {
+          title: "Star Wars",
+          description: "Scy-fiction movie about jedi and sith",
+          link: "https://es.wikipedia.org/wiki/Star_Wars",
+        },
+      ],
+    };
+    await axios
+      .post("http://localhost:5000/api/favs/create", favList)
+      .catch((error) => {
+        const status = error.response.status;
+        const message = error.response.data;
+        expect(status).toEqual(403);
+        expect(message).toEqual("Missing  Bearer Auth (JWT)");
+      });
+  });
+
   it("should get all listFav for all Users", async () => {
     const result = await axios.get("http://localhost:5000/api/favs", {
       headers: {
@@ -48,6 +70,15 @@ describe("ListFav tests", () => {
       },
     });
     expect(result.status).toEqual(200);
+  });
+
+  it("should fail getting all listFav for all Users for missing token", async () => {
+    await axios.get("http://localhost:5000/api/favs").catch((error) => {
+      const status = error.response.status;
+      const message = error.response.data;
+      expect(status).toEqual(403);
+      expect(message).toEqual("Missing  Bearer Auth (JWT)");
+    });
   });
 
   it("should get all listFav for a specific User", async () => {
@@ -63,6 +94,18 @@ describe("ListFav tests", () => {
     expect(result.status).toEqual(200);
   });
 
+  it("should fail getting all listFav for a specific User for missing token", async () => {
+    // User id 6255a4e06a9793184425c922 from esteban16.rodas@gmail.com
+    await axios
+      .get("http://localhost:5000/api/favs/6255a4e06a9793184425c922")
+      .catch((error) => {
+        const status = error.response.status;
+        const message = error.response.data;
+        expect(status).toEqual(403);
+        expect(message).toEqual("Missing  Bearer Auth (JWT)");
+      });
+  });
+
   it("should get one listFav by list Id", async () => {
     // FavList id 6271f94f583bd81d769f00d7 from esteban16.rodas@gmail.com
     const result = await axios.get(
@@ -74,6 +117,18 @@ describe("ListFav tests", () => {
       }
     );
     expect(result.status).toEqual(200);
+  });
+
+  it("should fail getting one listFav by list Id for missing token", async () => {
+    // FavList id 6271f94f583bd81d769f00d7 from esteban16.rodas@gmail.com
+    await axios
+      .get("http://localhost:5000/api/favs/singlelist/6271f94f583bd81d769f00d7")
+      .catch((error) => {
+        const status = error.response.status;
+        const message = error.response.data;
+        expect(status).toEqual(403);
+        expect(message).toEqual("Missing  Bearer Auth (JWT)");
+      });
   });
 
   it("should add items to one FavList", async () => {
@@ -100,6 +155,47 @@ describe("ListFav tests", () => {
     );
     expect(result.status).toEqual(200);
     expect(result.data).toBeDefined();
+  });
+
+  it("should fail add items to one FavList for missing token", async () => {
+    // Search listFav id to test
+    const listFavs = await axios.get("http://localhost:5000/api/favs", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const listFavToUpdate = listFavs.data[1];
+    const fav = {
+      title: "Star Wars II",
+      description: "Continue the Scy-fiction movie about jedi and sith",
+      link: "https://es.wikipedia.org/wiki/Star_Wars",
+    };
+    await axios
+      .put(`http://localhost:5000/api/favs/update/${listFavToUpdate._id}`, fav)
+      .catch((error) => {
+        const status = error.response.status;
+        const message = error.response.data;
+        expect(status).toEqual(403);
+        expect(message).toEqual("Missing  Bearer Auth (JWT)");
+      });
+  });
+
+  it("should fail deleting one FavList for missing token", async () => {
+    // Search favList id to delete
+    const listFavs = await axios.get("http://localhost:5000/api/favs", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const listFavToDelete = listFavs.data[1];
+    await axios
+      .delete(`http://localhost:5000/api/favs/delete/${listFavToDelete._id}`)
+      .catch((error) => {
+        const status = error.response.status;
+        const message = error.response.data;
+        expect(status).toEqual(403);
+        expect(message).toEqual("Missing  Bearer Auth (JWT)");
+      });
   });
 
   it("should delete one FavList", async () => {
--- api/controllers/lists.favs.controller.js
+++ api/controllers/lists.favs.controller.js
@@ -2,87 +2,30 @@ import { ListFav } from "../models/index.js";
 
 // Controller get all ListsFavs for all users  ************
 export const getAllListsFavs = async (req, res) => {
-  try {
-    const lists = await ListFav.find();
-    if (ListFav.length === 0) res.status(204).send();
-    else res.status(200).json(lists);
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+  const resp = ListFav.getAllListsFavs(req, res);
 };
 
 // Controller get all ListFav by user ******
 export const getListsFavsByUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const lists = await ListFav.find({ user_id: id });
-    res.status(200).json({ message: "success", lists });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
+  const resp = ListFav.getListsFavsByUser(req, res);
 };
 
 // Controller and specific ListFav for an user by ListFav_ID******
 export const getOneListFav = async (req, res) => {
-  try {
-    const { id: idList } = req.params;
-    const list = await ListFav.find({ _id: idList });
-    res.json(list);
-  } catch (error) {
-    res.status(403).json({ error });
-  }
+  const resp = ListFav.getOneListFav(req, res);
 };
 
 // Controller create one ListFav *******
 export const createListFav = async (req, res) => {
-  try {
-    const list = new ListFav(req.body);
-    const { name } = req.body;
-    const newList = await list.save();
-    newList &&
-      res
-        .status(201)
-        .json({ message: `list ${name} created successfully`, newList });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
+  const resp = ListFav.createListFav(req, res);
 };
 
-// Controller update a ListFav or add a new fav in list ****
+// Controller update a ListFav add a new fav in list ****
 export const addFavToList = async (req, res) => {
-  try {
-    const favToUpdate = req.body;
-    const { id: idListsFav } = req.params;
-    const list = await ListFav.findById(idListsFav);
-    if (list && favToUpdate) {
-      list.favs.push(favToUpdate);
-      list &&
-        ListFav.updateOne({ _id: list._id }, list, null, (error, result) => {
-          !error
-            ? res.status(200).json({ message: "success", result })
-            : res.status(500).send({ message: "Internal server error" });
-        });
-    } else return res.status(204).json({ message: "ListFav not found" });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
+  const resp = ListFav.addFavToList(req, res);
 };
 
 // Controller delete a ListFav ******
 export const deleteListFav = async (req, res) => {
-  const { id: idList } = req.params;
-  try {
-    const listToDelete = await ListFav.findById(idList);
-    const { name } = listToDelete;
-
-    if (!listToDelete) {
-      res.status(204).send({ err: "No list to delete" });
-    } else {
-      const deletedList = await ListFav.deleteOne(listToDelete);
-      if (deletedList)
-        res.status(200).send(`ListFav ${name} deleted Successfully`);
-    }
-  } catch (error) {
-    res.status(403).send();
-  }
+  const resp = ListFav.deleteListFav(req, res);
 };
